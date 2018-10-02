5c73024755937d3095bc18e219fba252c2096c09
RodrigoGustavo <rodrigogustavo@gmail.com>
2018-10-01 22:31:03 -0400

    get data via restful api - method get.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -7,13 +8,33 @@ import './Blog.css';
 
 class Blog extends Component {
 
+    state = {
+        posts: []
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map(post =>  {
+                return {
+                    ...post,
+                    author: 'Rodrigo'
+                }
+            });
+
+            this.setState({posts: updatedPosts})
+        });
+    }
+
     render () {
+        const posts = this.state.posts.map( post => {
+            return <Post keu={post.id} title={post.title} author={post.author} />
+        })
+        
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
